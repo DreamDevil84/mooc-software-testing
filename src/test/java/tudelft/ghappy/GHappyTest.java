d0355a762696ced9e4eb48d558c48e6ac6e3303d
Daniel Edström <daniel.edstrom1984@hotmail.com>
2021-01-27 12:57:01 +0100

    Practical exercise 1 done

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,29 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+
+    private GHappy gHappy;
+
+    @BeforeEach
+    public void initialize() {this.gHappy = new GHappy();}
+
+    @ParameterizedTest(name = "{0}")
+    @CsvSource({"xxggxx","ggxx","xxgg"})
+    public void gHappyTestTrue(String string){
+        boolean result = gHappy.gHappy(string);
+        Assertions.assertTrue(result);
+    }
+
+
+    @ParameterizedTest(name = "{0}")
+    @CsvSource({"xxgxx","xxggyygxx","gxx","xxg"})
+    public void gHappyTestFalse(String string){
+        boolean result = gHappy.gHappy(string);
+        Assertions.assertFalse(result);
+    }
 }
--- src/test/java/tudelft/leapyear/LeapYearTest.java
+++ src/test/java/tudelft/leapyear/LeapYearTest.java
@@ -1,6 +1,7 @@
 package tudelft.leapyear;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class LeapYearTest {
