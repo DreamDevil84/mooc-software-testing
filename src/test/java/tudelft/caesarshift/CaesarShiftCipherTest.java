d0f1c755dd7abe9bb8684d7dc4cafd92f7351fa2
Daniel Edström <daniel.edstrom1984@hotmail.com>
2021-01-27 13:12:14 +0100

    Practical exercise 2 done

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,24 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class CaesarShiftCipherTest {
 
+    @ParameterizedTest(name = "input={0},output={1}")
+    @CsvSource({"abc,def","xyz,abc"})
+    public void caesarShiftCipherTest(String input, String output){
+        CaesarShiftCipher caesar = new CaesarShiftCipher();
+        String result = caesar.CaesarShiftCipher(input, 3);
+        Assertions.assertEquals(output, result);
+    }
+
+    @Test
+    public void invalidInput(){
+        CaesarShiftCipher caesar = new CaesarShiftCipher();
+        String result = caesar.CaesarShiftCipher("ABC", 3);
+        Assertions.assertEquals("invalid", result);
+    }
 }
