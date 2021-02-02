a376425377fb726db94c466fbbda36d77bf28918
Daniel Edström <daniel.edstrom1984@hotmail.com>
2021-02-02 12:57:16 +0100

    Mirror exercise done

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,14 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 public class DiscountApplierTest {
 
+    @Test
+    public void discountTestWithMock(){
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+
+    }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,19 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class MirrorTest {
+
+    @ParameterizedTest(name = "Input = {0}, Output = {1}")
+    @CsvSource({"aba,aba","abba,abba","abXYZba,ab"})
+    public void mirrorTest(String string, String expected){
+        Mirror mirror = new Mirror();
+
+        String result = mirror.mirrorEnds(string);
+
+        Assertions.assertEquals(expected, result);
+    }
+
 }
